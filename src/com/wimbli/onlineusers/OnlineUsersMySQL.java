59d4e4c390d4535b6b80cb4f31437042a1a72863
Brettflan <dsr@fenrisoft.com>
2012-04-18 22:58:32 -0500

    IMPORTANT: BREAKING CHANGE for MySQL users: the time_total column type is changed from TIME to INT. The new INT values refer to the number of seconds the user has been online. This was necessary due to TIME columns being limited to roughly 839 hours before being truncated.
    Any SQL queries you've written which reference the time_total column will likely need to be updated as a result. For a quick way to get a TIME value like before, you can use something like the SQL "SEC_TO_TIME(`time_total`)" or similar, but again you should note that SQL TIME values are limited to ~839 hours before they stop working correctly.

--- src/com/wimbli/onlineusers/OnlineUsersMySQL.java
+++ src/com/wimbli/onlineusers/OnlineUsersMySQL.java
@@ -16,16 +16,23 @@ public class OnlineUsersMySQL extends OnlineUsersDataSource {
     private static String sqlMakeTable 		   = "CREATE TABLE IF NOT EXISTS `"+OnlineUsers.table+"` ("+
     												"`name` varchar(32) NOT NULL, " +
     												"`time` datetime DEFAULT NULL, " +
+    												"`time_total` int DEFAULT 0, " +
     												"PRIMARY KEY (`name`))";
-    private static String sqlCheckTableExist   = "SHOW TABLES LIKE '"+OnlineUsers.table+"'";
-    private static String sqlAlterTableOnline  = "ALTER TABLE `"+OnlineUsers.table+"` ADD `online` bit(1) NOT NULL DEFAULT 0";
-    private static String sqlAlterTableOnline2 = "ALTER TABLE `"+OnlineUsers.table+"` CHANGE  `online`  `online` TINYINT( 1 ) UNSIGNED NOT NULL DEFAULT  '0'";
-    private static String sqlAlterTableOnline3 = "ALTER TABLE `"+OnlineUsers.table+"` ADD `time_total` TIME NOT NULL DEFAULT 0";
     private static String sqlOnlineUser 	   = "INSERT INTO `"+OnlineUsers.table+"` (`name`, `time`, `online`) VALUES (?, NOW(), 1) ON DUPLICATE KEY UPDATE `time`=NOW(), `online`=1";
-    private static String sqlOfflineUser 	   = "UPDATE `"+OnlineUsers.table+"` SET `time_total` = IF(`online`=1, ADDTIME(`time_total`, TIMEDIFF(NOW(), `time`)), `time_total`), `online`=0 WHERE `name`=?";
+    private static String sqlOfflineUser 	   = "UPDATE `"+OnlineUsers.table+"` SET `time_total` = IF(`online`=1, `time_total` +  TIMESTAMPDIFF(SECOND, `time`, NOW()), `time_total`), `online`=0 WHERE `name`=?";
     private static String sqlDeleteOfflineUser = "DELETE FROM `"+OnlineUsers.table+"` WHERE `name`=?";
-    private static String sqlSetAllOffline     = "UPDATE `"+OnlineUsers.table+"` SET `time_total` = IF(`online`=1, ADDTIME(`time_total`, TIMEDIFF(NOW(), `time`)), `time_total`), `online`=0";
-    
+    private static String sqlSetAllOffline     = "UPDATE `"+OnlineUsers.table+"` SET `time_total` = IF(`online`=1, `time_total` +  TIMESTAMPDIFF(SECOND, `time`, NOW()), `time_total`), `online`=0";
+
+	// these are run see if update for older databases is needed, and then update them if so
+    private static String sqlCheckTableExist   = "SHOW TABLES LIKE '"+OnlineUsers.table+"'";
+    private static String sqlCheckTableTimeTt  = "SHOW COLUMNS FROM `"+OnlineUsers.table+"` WHERE `Field` = 'time_total'";
+    private static String sqlAlterTableOnline  = "ALTER TABLE `"+OnlineUsers.table+"` ADD `online` bit(1) NOT NULL DEFAULT 0";
+    private static String sqlAlterTableOnline2 = "ALTER TABLE `"+OnlineUsers.table+"` CHANGE  `online`  `online` TINYINT( 1 ) UNSIGNED NOT NULL DEFAULT  0";
+    private static String sqlAlterTableTimeTtA = "ALTER TABLE `"+OnlineUsers.table+"` ADD `time_total` INT NOT NULL DEFAULT 0";
+    private static String sqlAlterTableTimeTt1 = "ALTER TABLE `"+OnlineUsers.table+"` CHANGE `time_total` `time_total_old` TIME NOT NULL DEFAULT '00:00:00'";
+    private static String sqlAlterTableTimeTt2 = "ALTER TABLE `"+OnlineUsers.table+"` ADD `time_total` INT NOT NULL DEFAULT 0";
+    private static String sqlAlterTableTimeTt3 = "UPDATE `"+OnlineUsers.table+"` SET `time_total` = TIME_TO_SEC(`time_total_old`)";
+    private static String sqlAlterTableTimeTt4 = "ALTER TABLE `"+OnlineUsers.table+"` DROP `time_total_old`";
 	
 	
 	@Override
@@ -127,30 +134,53 @@ public class OnlineUsersMySQL extends OnlineUsersDataSource {
 	private boolean createTable() {
 		Connection conn = null;
         Statement s = null;
+		ResultSet rs = null;
         try {
         	conn = getConnection();
         	s = conn.createStatement();
         	s.executeUpdate(sqlMakeTable);
+			// Run sqlCheckTableTimeTt query to see if time_total column exists, and check column type
         	try {
-        		s.executeUpdate(sqlAlterTableOnline3);
+	        	rs = s.executeQuery(sqlCheckTableTimeTt);
+	        	if (rs.first()) {
+					// `time_total` column exists, but does it need to be altered from TIME to INT?
+					if (!rs.getString("Type").toLowerCase().startsWith("int"))
+					{
+		        		log.info(name + ": Updating Table, changing time_total column from TIME to INT");
+						// sadly altering a column directly from TIME to INT will reset all values to 0, so out of necessity,
+						// we first rename the column, then create a new one with the new type, then translate the values over, then delete the original column to clean up
+		        		s.executeUpdate(sqlAlterTableTimeTt1);
+		        		s.executeUpdate(sqlAlterTableTimeTt2);
+		        		s.executeUpdate(sqlAlterTableTimeTt3);
+		        		s.executeUpdate(sqlAlterTableTimeTt4);
+					}
+					rs.close();
+					s.close();
+					conn.close();
+					return true;
+				}
         		log.info(name + ": Updating Table");
+        		s.executeUpdate(sqlAlterTableTimeTtA);
         		s.executeUpdate(sqlAlterTableOnline);
         		s.executeUpdate(sqlAlterTableOnline2);
         	} catch (SQLException ex2){}
-        	ResultSet rs = s.executeQuery(sqlCheckTableExist);
+        	rs = s.executeQuery(sqlCheckTableExist);
         	if (rs.first()) {
+				rs.close();
+				s.close();
+				conn.close();
         		return true;
         	}
         } catch (SQLException ex) {
         	log.severe(name + ": " + ex.getMessage());
         } finally {
             try {
-                if (s != null) {
+				if (rs != null)
+					rs.close();
+                if (s != null)
                     s.close();
-                }
-                if (conn != null) {
+                if (conn != null)
                     conn.close();
-                }
             } catch (SQLException ex) {
             	log.severe(name + ": " + ex.getMessage());
             }
